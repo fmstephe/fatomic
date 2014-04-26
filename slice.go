74650e22743c1cadce9b244fdd13d22b34c9be94
Francis Stephens <francisstephens@gmail.com>
2014-04-26 23:00:26 +0100

    Added CacheProtectedPointers() function
    This allows us to create slices of unsafe.Pointer without fear of false
    sharing.

--- slice.go
+++ slice.go
@@ -1,8 +1,17 @@
 package fatomic
 
+import (
+	"unsafe"
+)
+
 const cacheLen = 64
 
 func CacheProtectedBytes(size int) []byte {
 	b := make([]byte, cacheLen + size + cacheLen)
+	return b[cacheLen:size+cacheLen]
+}
+
+func CacheProtectedPointers(size int) []unsafe.Pointer {
+	b := make([]unsafe.Pointer, cacheLen + size + cacheLen)
 	return b[cacheLen:size+cacheLen]
 }
